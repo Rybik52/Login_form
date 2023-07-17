ae57fa15df07b2b409e3548a458e6699059e8c0b
Rybik <endyse70@gmail.com>
2023-07-17 14:25:25 +0300

    first commit

---
+++ validation.js
@@ -0,0 +1,19 @@
+function validatePassword() {
+    var p = document.querySelector('input[name="password"]').value,
+        errors = [];
+    if (p.length < 8) {
+        errors.push("Your password must be at least 8 characters"); 
+    }
+    if (p.search(/[a-z]/i) < 0) {
+        errors.push("Your password must contain at least one letter.");
+    }
+    if (p.search(/[0-9]/) < 0) {
+        errors.push("Your password must contain at least one digit."); 
+    }
+    if (errors.length > 0) {
+        alert(errors.join("\n"));
+        return false;
+    }
+    return true;
+}
+validatePassword()
